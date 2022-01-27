f325d093b11296ada7875fc07169e25f016d245c
Faizan <alifazian@gmail.com>
2022-01-27 14:58:36 +0500

    change setting

--- stexity/settings.py
+++ stexity/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n4(3!opm-+bjs#1vk1$633e9v7a$5(g258v293av-0)!509@*p'
+# SECRET_KEY = 'django-insecure-n4(3!opm-+bjs#1vk1$633e9v7a$5(g258v293av-0)!509@*p'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
