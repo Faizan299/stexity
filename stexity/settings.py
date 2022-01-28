b9eca0e15c294f8a4aeb44c3e0bd1e2d1df6e18a
Faizan <alifazian@gmail.com>
2022-01-28 15:06:08 +0500

    Changes apply setting file

--- stexity/settings.py
+++ stexity/settings.py
@@ -82,10 +82,17 @@ WSGI_APPLICATION = 'stexity.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'OPTIONS': {
+            'service': 'my_service',
+            'passfile': '.my_pgpass',
+        },
     }
 }
+
+import dj_database_url
+db_form_env = dj_database_url.config(conn_max_age=600)
+DATABASES['defualt'].update(db_form_env)
 
 
 # Password validation
